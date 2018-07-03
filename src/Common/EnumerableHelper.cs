80898ef5076f5e27879da1d26eff10af93ccc774
gongqifeng <yjy04@zhengyee.com>
2018-07-03 15:00:13 +0800

    Add EnumerableHelper class

---
+++ src/Common/EnumerableHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PracticalPatterns.Common
+{
+    public class EnumerableHelper
+    {
+        public static void ForEach<T>(this IEnumerable<T> sequence, Action<T> action)
+        {
+            if (sequence == null)
+            {
+                throw new ArgumentException("sequence");
+            }
+            if (action == null)
+            {
+                throw new ArgumentException("action");
+            }
+            foreach (var item in sequence)
+            {
+                action(item);
+            }
+        }
+    }
+}
