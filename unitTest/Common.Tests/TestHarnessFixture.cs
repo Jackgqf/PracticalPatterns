361492344cf63c937628aebad2b2f0fa1f027c01
qifeng gong <gongqifeng.vip@gmail.com>
2018-07-03 16:02:24 +0800

    添加对多线程代码的单元测试(Add the unit test code to MultiThread sample code)

---
+++ unitTest/Common.Tests/TestHarnessFixture.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PracticalPatterns.Common.Tests
+{
+    [TestClass]
+    public class TestHarnessFixture
+    {
+        IList<string> recoder = new List<string>();
+
+        [TestMethod]
+        public void TestParallelExecution()
+        {
+            var start = DateTime.Now;
+            TestHarness.Parallel
+                (
+                    () => { Thread.Sleep(2000); recoder.Add("A"); },
+                    () => { Thread.Sleep(3000); recoder.Add("B"); },
+                    () => { Thread.Sleep(1500); recoder.Add("C"); }
+                );
+            var end = DateTime.Now;
+
+            //max execution time span
+            Assert.AreEqual<int>(3, (end - start).Seconds);
+
+            //execution sequence
+            Assert.AreEqual<string>("C", recoder[0]);
+            Assert.AreEqual<string>("A", recoder[1]);
+            Assert.AreEqual<string>("B", recoder[2]);
+        }
+    }
+}
