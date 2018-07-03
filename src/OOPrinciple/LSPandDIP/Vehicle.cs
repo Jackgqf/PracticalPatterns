4cec75712fd2516ae80923a26b1ba081e90eafe6
qifeng gong <gongqifeng.vip@gmail.com>
2018-07-03 17:01:00 +0800

    添加里氏替换原则和依赖倒置原则示例(Add LSP and DIP sample code)

---
+++ src/OOPrinciple/LSPandDIP/Vehicle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PracticalPatterns.OOPrinciple.LSPandDIP
+{
+    interface IVehicle
+    {
+        void Run();
+    }
+
+    class Bicycle : IVehicle
+    {
+        public void Run()
+        {
+            //Run like a bicycle
+        }
+    }
+
+    class Train : IVehicle
+    {
+        public void Run()
+        {
+            //Run like a train
+        }
+    }
+
+    class Client
+    {
+        public void ShowAVehicleRun(IVehicle vehicle)
+        {
+            vehicle.Run();
+        }
+    }
+}
