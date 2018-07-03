1a17b63308854bddc26e093a0b1604bd76e0ef73
gongqifeng <yjy04@zhengyee.com>
2018-07-03 15:30:27 +0800

    Add the unit test project of EnumerableHelper and add it to solution

---
+++ unitTest/Common.Tests/EnumerableHelperFixture.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using PracticalPatterns.Common;
+
+namespace PracticalPatterns.Common.Tests
+{
+    [TestClass]
+    public class EnumerableHelperFixture
+    {
+        [TestMethod]
+        public void TestForEach()
+        {
+            string[] data = { "A", "B", "CDE", "FG" };
+            int[] lens = new int[data.Length];
+            int i = 0;
+            EnumerableHelper.ForEach<string>(data,
+                delegate(string item)
+                {
+                    lens[i++] = item.Length;
+                    Trace.WriteLine(lens[i - 1]);
+                });
+            Assert.AreEqual<int>(string.Join("", data).Length, lens.Sum());
+        }
+    }
+}
