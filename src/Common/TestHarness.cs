eec3b8d8ff264892d0f8d22e0c47e2d120732b67
qifeng gong <gongqifeng.vip@gmail.com>
2018-07-03 16:01:19 +0800

    添加多线程示例代码(Add MutiThread sample code)

---
+++ src/Common/TestHarness.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading;
+
+namespace PracticalPatterns.Common
+{
+    public static class TestHarness
+    {
+        public static void Parallel(params ThreadStart[] actions)
+        {
+            Thread[] threads = actions.Select(a => new Thread(a)).ToArray();
+            threads.ForEach(t => t.Start());
+            threads.ForEach(t => t.Join());
+        }
+    }
+}
