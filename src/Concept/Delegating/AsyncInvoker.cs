e86f0ffed4cfc1607c61f9afd42b0ed05aa131f8
qifeng gong <gongqifeng.vip@gmail.com>
2018-07-04 10:24:46 +0800

    添加委托异步调用示例(Add a sample to asyncInvoke with delegate)

---
+++ src/Concept/Delegating/AsyncInvoker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace PracticalPatterns.Concept.Delegating
+{
+    public class AsyncInvoker
+    {
+        static void OnTimerInterval(object state)
+        {
+            Trace.WriteLine(state as string);
+        }
+
+        public AsyncInvoker()
+        {
+            new Timer(new TimerCallback(OnTimerInterval), "slow", 2500, 2500);
+            new Timer(new TimerCallback(OnTimerInterval), "fast", 2000, 2000);
+            Trace.WriteLine("method");
+        }
+    }
+}
