fd5b13886a09d079608f76ea6d2a96b495282676
qifeng gong <gongqifeng.vip@gmail.com>
2018-07-11 13:59:02 +0800

    a simple sample of generic apply to pattern(一个简单地泛型应用于模式的示例)

---
+++ src/Concept/Generics/RawGenericFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PracticalPatterns.Concept.Generics
+{
+    public class RawGenericFactory<T>
+    {
+        public T Create(string typeName)
+        {
+            if (string.IsNullOrEmpty(typeName))
+            {
+                throw new ArgumentNullException("typeName");
+            }
+            return (T)Activator.CreateInstance(Type.GetType(typeName));
+        }
+    }
+}
---
+++ unitTest/Concept.Tests/RawGenericFactoryFixture.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PracticalPatterns.Concept.Generics;
+using System.Diagnostics;
+
+namespace PracticalPatterns.Concept.Tests
+{
+    [TestClass]
+    public class RawGenericFactoryFixture
+    {
+        interface IProduct { }
+        class ConcreateProduct : IProduct { }
+
+        interface IUser { }
+        class ConcreateUserA : IUser { }
+
+        [TestMethod]
+        public void Test()
+        {
+            string typeName = typeof(ConcreateProduct).AssemblyQualifiedName;
+            Trace.WriteLine(typeName);
+            Trace.WriteLine(typeof(ConcreateProduct));
+
+            var product = new RawGenericFactory<ConcreateProduct>().Create(typeName);
+            Assert.IsNotNull(product);
+            Assert.IsInstanceOfType(product, typeof(ConcreateProduct));
+            Assert.IsTrue(product is IProduct);
+
+            var user = new RawGenericFactory<ConcreateUserA>().Create(typeof(ConcreateUserA).AssemblyQualifiedName);
+            Assert.IsNotNull(user);
+            Assert.IsInstanceOfType(user, typeof(ConcreateUserA));
+            Assert.IsTrue(user is IUser);
+        }
+    }
+}
