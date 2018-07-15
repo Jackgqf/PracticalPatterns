0b3505385cdadfd6d8dbd5e7fc8092d0552856f7
qifeng gong <gongqifeng.vip@gmail.com>
2018-07-15 16:04:47 +0800

    Add the sample of overloadable operators and conversion operators

---
+++ src/Concept/Operator/Season.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PracticalPatterns.Concept.Operator
+{
+    public class Season
+    {
+        public static readonly string[] seasons =
+            new string[] { "Spring", "Summer", "Autumn", "Winter" };
+        int current;
+        public Season()
+        {
+            current = default(int);
+        }
+        public static Season operator ++(Season season)
+        {
+            season.current = (season.current + 1) % 4;
+            return season;
+        }
+        public override string ToString()
+        {
+            return seasons[current];
+        }
+
+        public static implicit operator string(Season season)
+        {
+            return season.ToString();
+        }
+    }
+}
---
+++ unitTest/Concept.Tests/SeasonFixture.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PracticalPatterns.Concept.Operator;
+using System.Diagnostics;
+
+namespace PracticalPatterns.Concept.Tests
+{
+    [TestClass]
+    public class SeasonFixture
+    {
+        [TestMethod]
+        public void TestSeasonIterator()
+        {
+            var season = new Season();
+
+            Assert.AreEqual<string>(Season.seasons[0], season);
+            Trace.WriteLine(season);
+
+            season++;
+            season++;
+
+            Assert.AreEqual<string>(Season.seasons[2], season);
+            Trace.WriteLine(season);
+        }
+    }
+}
