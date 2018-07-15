ba76bc0fb93a4366119e438de286958cae946262
qifeng gong <gongqifeng.vip@gmail.com>
2018-07-15 16:33:40 +0800

    add a sample which operate collection type with overload operators

---
+++ src/Concept/Operator/ErrorEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PracticalPatterns.Concept.Operator
+{
+    public class ErrorEntity
+    {
+        private IList<string> messages = new List<string>();
+        private IList<int> codes = new List<int>();
+
+        public IList<string> Messages { get { return messages; } }
+        public IList<int> Codes { get { return codes; } }
+
+        public static ErrorEntity operator +(ErrorEntity errorEntity, string message)
+        {
+            errorEntity.messages.Add(message);
+            return errorEntity;
+        }
+
+        public static ErrorEntity operator +(ErrorEntity errorEntity, int code)
+        {
+            errorEntity.Codes.Add(code);
+            return errorEntity;
+        }
+    }
+}
---
+++ unitTest/Concept.Tests/ErrorEntityFixture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PracticalPatterns.Concept.Operator;
+using System.Diagnostics;
+
+namespace PracticalPatterns.Concept.Tests
+{
+    [TestClass]
+    public class ErrorEntityFixture
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var entity = new ErrorEntity();
+            entity += "user name cannot be null";
+            entity += "password cannot be null";
+            entity += "error\n";
+            entity += 1;
+            entity += 2;
+            Assert.AreEqual(3, entity.Messages.Count);
+            Assert.AreEqual(2, entity.Codes.Count);
+            Trace.WriteLine(String.Join("\n", entity.Messages));
+        }
+    }
+}
