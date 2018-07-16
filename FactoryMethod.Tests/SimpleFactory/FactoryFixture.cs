ffcca5ec623f53960cb028399beea2715358ab27
qifeng gong <gongqifeng.vip@gmail.com>
2018-07-16 08:49:44 +0800

    Add factory method pattern(添加工厂&工厂方法模式)

---
+++ FactoryMethod.Tests/SimpleFactory/FactoryFixture.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PracticalPatterns.FactoryMethod.SimpleFactory;
+
+namespace PracticalPatterns.FactoryMethod.Tests.SimpleFactory
+{
+    [TestClass]
+    public class FactoryFixture
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Assert.IsInstanceOfType(new Factory().Create(), typeof(ConcreteProductA));
+        }
+    }
+}
---
+++ FactoryMethod/SimpleFactory/IProduct.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PracticalPatterns.FactoryMethod.SimpleFactory
+{
+    public interface IProduct { }
+    public class ConcreteProductA : IProduct { }
+    public class ConcreteProductB : IProduct { }
+
+    public class Factory
+    {
+        public IProduct Create()
+        {
+            return new ConcreteProductA();
+        }
+    }
+}
