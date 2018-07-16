3e99175ec941537efd6496fc0527c13b6ee11449
qifeng gong <gongqifeng.vip@gmail.com>
2018-07-16 08:07:30 +0800

    add a inject way by interface

---
+++ unitTest/Concept.Tests/DependencyInjection/Interfacer/InterfaceInjectionFixture.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PracticalPatterns.Concept.DependencyInjection;
+
+namespace PracticalPatterns.Concept.Tests.DependencyInjection.Interfacer
+{
+    [TestClass]
+    public class InterfaceInjectionFixture
+    {
+        class Client<T> : ITimeProvider
+            where T : ITimeProvider
+        {
+            public T Provider { get; set; }
+
+            public DateTime CurrentDate
+            {
+                get { return Provider.CurrentDate; }
+            }
+        }
+
+        [TestMethod]
+        public void Test()
+        {
+            var client = new Client<ITimeProvider>(){Provider = new Assembler().Create<ITimeProvider>()};
+
+            Assert.IsNotNull(client);
+            Assert.IsInstanceOfType(client.Provider, typeof(SystemTimeProvider));
+
+            Assert.IsInstanceOfType(client.Provider.CurrentDate, typeof(DateTime));
+            Assert.IsTrue(typeof(ITimeProvider).IsAssignableFrom(client.GetType()));
+        }
+    }
+}
---
+++ unitTest/Concept.Tests/DependencyInjection/Interfacer/InterfaceInjectionTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PracticalPatterns.Concept.DependencyInjection;
+
+namespace PracticalPatterns.Concept.Tests.DependencyInjection.Interfacer
+{
+    interface IobjectWithTimeProvider
+    {
+        ITimeProvider TimeProvider { get; set; }
+    }
+
+    class Client : IobjectWithTimeProvider
+    {
+        public ITimeProvider TimeProvider { get; set; }
+    }
+
+    [TestClass]
+    public class TestClient
+    {
+        [TestMethod]
+        public void Test()
+        {
+            ITimeProvider timeProvider = new Assembler().Create<ITimeProvider>();
+            Assert.IsNotNull(timeProvider);
+            IobjectWithTimeProvider objectWithProvider = new Client();
+            objectWithProvider.TimeProvider = timeProvider;
+        }
+    }
+    
+}
