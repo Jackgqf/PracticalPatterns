49b47b5cee9a997387cec77c890a0e4a44e23fdb
qifeng gong <gongqifeng.vip@gmail.com>
2018-07-17 16:42:47 +0800

    Add app.config file and make a simple unit test

---
+++ unitTest/FactoryMethod.Tests/Classic/ConfigurationFactoryFixture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace PracticalPatterns.FactoryMethod.Tests.Classic
+{
+    [TestClass]
+    public class ConfigurationFactoryFixture
+    {
+        const string SectionName = "factoryMethod.customFactories";
+        const string Name = "PracticalPatterns.FactoryMethod.Tests.Classic.IFactory,FactoryMethod.Tests";
+
+        [TestMethod]
+        public void Test()
+        {
+            string typeName = ((NameValueCollection)ConfigurationManager.GetSection(SectionName))[Name];
+            Assert.IsTrue(typeof(IFactory).IsAssignableFrom(Type.GetType(typeName)));
+        }
+    }
+}
