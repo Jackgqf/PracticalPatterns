caa9e1cffc58620adaef10e186dc2a639a002116
qifeng gong <gongqifeng.vip@gmail.com>
2018-07-17 21:16:19 +0800

    Implement the IEnumerable interface for factory(为工厂实现迭代器)

--- src/FactoryMethod/Generics/IFactory.cs
+++ src/FactoryMethod/Generics/IFactory.cs
@@ -44,4 +44,41 @@ namespace PracticalPatterns.FactoryMethod.Generics
             
         }
     }
+
+    public interface IBatchFactory : IFactory
+    {
+        IEnumerable<IProduct> Create(string name, int quantity);
+        IEnumerable<IProduct> Create(int quantity);
+    }
+
+    public class BatchFactory : Factory, IBatchFactory
+    {
+        IEnumerable<T> InternalCreate<T>(Type type, int quantity)
+        {
+            if (quantity <= 0) throw new IndexOutOfRangeException("quantity");
+            if (type == null) throw new ArgumentNullException("type");
+            IList<T> result = new List<T>();
+            for (int i = 0; i < quantity; i++)
+            {
+                result.Add((T)Activator.CreateInstance(type));
+            }
+            return result;
+        }
+       
+        public IEnumerable<IProduct> Create(int quantity)
+        {
+            if (quantity <= 0)
+            {
+                throw new IndexOutOfRangeException("quantity");
+            }
+            return InternalCreate<IProduct>(productRegistry[DefaultName], quantity);
+        }
+
+        public IEnumerable<IProduct> Create(string name, int quantity)
+        {
+            if (string.IsNullOrEmpty(name)) throw new ArgumentNullException("name");
+            if (quantity <= 0) throw new IndexOutOfRangeException("quantity");
+            return InternalCreate<IProduct>(productRegistry[name], quantity);
+        }
+    }
 }
--- unitTest/FactoryMethod.Tests/GenericsFactoryFixture.cs
+++ unitTest/FactoryMethod.Tests/GenericsFactoryFixture.cs
@@ -20,5 +20,27 @@ namespace PracticalPatterns.FactoryMethod.Tests
             Assert.IsInstanceOfType(factory.Create("a"), typeof(ProductA));
             Assert.IsInstanceOfType(factory.Create("b"), typeof(ProductB));
         }
+
+        [TestMethod]
+        public void BatchCreateProductByName()
+        {
+            var batchSize = 5;
+            var factory = new BatchFactory();
+
+            //create default product 
+            var products = factory.Create(batchSize);
+            Assert.AreEqual<int>(batchSize, products.Count());
+            Assert.AreEqual<int>(batchSize, products.Count(x => x is ProductA));
+            
+            //concrete product type by name
+            products = factory.Create("a", batchSize);
+            Assert.AreEqual<int>(batchSize, products.Count());
+            Assert.AreEqual<int>(batchSize, products.Count(x => x is ProductA));
+
+            products = factory.Create("b", batchSize);
+            Assert.AreEqual<int>(batchSize, products.Count());
+            Assert.AreEqual<int>(batchSize, products.Count(x => x is ProductB));
+
+        }
     }
 }
