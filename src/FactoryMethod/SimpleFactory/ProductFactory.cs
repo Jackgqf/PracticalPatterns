6cd2eae09186396c94ec7f01fba639b4c698a969
qifeng gong <gongqifeng.vip@gmail.com>
2018-07-17 11:53:27 +0800

    add a static class to support simple factory pattern（添加支持简单工厂的静态类）

---
+++ src/FactoryMethod/SimpleFactory/ProductFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PracticalPatterns.FactoryMethod.SimpleFactory
+{
+    public enum Category
+    {
+        A,
+        B
+    }
+
+    public static class ProductFactory
+    {
+        public static IProduct Create(Category categoty)
+        {
+            switch (categoty)
+            {
+                case Category.A:
+                    return new ConcreteProductA();
+                case Category.B:
+                    return new ConcreteProductB();
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+    }
+}
---
+++ unitTest/FactoryMethod.Tests/SimpleFactory/ProductFactoryFixture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PracticalPatterns.FactoryMethod.SimpleFactory;
+
+namespace PracticalPatterns.FactoryMethod.Tests.SimpleFactory
+{
+    [TestClass]
+    public class ProductFactoryFixture
+    {
+        [TestMethod]
+        public void TestCreate()
+        {
+            var product = ProductFactory.Create(Category.A);
+            Assert.IsNotNull(product);
+            Assert.IsInstanceOfType(product, typeof(ConcreteProductA));
+        }
+    }
+}
