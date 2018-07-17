6628c3d8b771e87ad5f9598b6ce8419e19085cf8
qifeng gong <gongqifeng.vip@gmail.com>
2018-07-17 20:14:26 +0800

    add app.config file to implement the factory method pattern wirh the way pass parameters

---
+++ src/FactoryMethod/Generics/IFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.Specialized;
+using System.Configuration;
+
+namespace PracticalPatterns.FactoryMethod.Generics
+{
+    public interface IFactory
+    {
+        IProduct Create();
+        IProduct Create(string name);
+    }
+
+    public class ProductRegistry
+    {
+        const string SectionName = "concreteProducts";
+        static NameValueCollection collection =
+            (NameValueCollection)ConfigurationManager.GetSection(SectionName);
+
+        public Type this[string name]
+        {
+            get { return Type.GetType(collection[name]); }
+        }
+    }
+
+    public class Factory : IFactory
+    {
+        public const string DefaultName = "DEFAULT";
+        public readonly ProductRegistry productRegistry = new ProductRegistry();
+
+        public IProduct Create()
+        {
+            return (IProduct)Activator.CreateInstance(productRegistry[DefaultName]);
+        }
+        public IProduct Create(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new NullReferenceException("name");
+            }
+            return (IProduct)Activator.CreateInstance(productRegistry[name]);
+            
+        }
+    }
+}
---
+++ unitTest/FactoryMethod.Tests/GenericsFactoryFixture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PracticalPatterns.FactoryMethod.Generics;
+
+namespace PracticalPatterns.FactoryMethod.Tests
+{
+    [TestClass]
+    public class GenericsFactoryFixture
+    {
+        [TestMethod]
+        public void CreateProductByName()
+        {
+            var factory = new Factory();
+
+            Assert.IsInstanceOfType(factory.Create(), typeof(ProductA));
+            Assert.IsInstanceOfType(factory.Create("a"), typeof(ProductA));
+            Assert.IsInstanceOfType(factory.Create("b"), typeof(ProductB));
+        }
+    }
+}
