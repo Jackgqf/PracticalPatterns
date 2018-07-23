87182248d4b75c77edb976e1672e8007c40573cf
qifeng gong <gongqifeng.vip@gmail.com>
2018-07-23 08:19:55 +0800

    add the unit test for classic builder pattern

---
+++ unitTest/Builder.Tests/ClassicBuilderFixture.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PracticalPatterns.Builder.Classic;
+
+namespace PracticalPatterns.Builder.Tests
+{
+    [TestClass]
+    public class ClassicBuilderFixture
+    {
+        [TestMethod]
+        public void TestBuilder()
+        {
+            var maker1 = new VehicleMaker();
+            maker1.Builder = new CarBuilder();
+            maker1.Construct();
+            Assert.AreEqual(4, maker1.Vehicle.Wheels.Count());
+            Assert.AreEqual(4, maker1.Vehicle.Lights.Count());
+
+            var maker2 = new VehicleMaker();
+            maker2.Builder = new BicycleBuilder();
+            maker2.Construct();
+            Assert.AreEqual(2, maker2.Vehicle.Wheels.Count());
+            Assert.IsNull(maker2.Vehicle.Lights);
+        }
+    }
+}
