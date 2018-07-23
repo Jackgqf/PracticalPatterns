507c06b5212187b80257893ade723ff5cb3f4071
qifeng gong <gongqifeng.vip@gmail.com>
2018-07-23 20:21:25 +0800

    a builder interface

---
+++ src/Builder/Attributed/IBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PracticalPatterns.Builder.Attributed
+{
+    public interface IBuilder<T> where T:class,new()
+    {
+        T BuildUp();
+    }
+
+    public class Builder<T> : IBuilder<T> where T : class,new()
+    {
+        public virtual T BuildUp()
+        {
+            var steps = new BuilderStepDiscovery().DiscoveryBuildSteps(typeof(T));
+            if (steps == null)
+            {
+                return new T(); 
+            }
+            var target = new T();
+            foreach (var step in steps)
+            {
+                for (var i = 0; i < step.Times; i++)
+                {
+                    step.Method.Invoke(target, null);
+                }
+            }
+            return target;
+        }
+    }
+}
