759c864d8ef36a67ae38320ccd99303a1cbeabe2
qifeng gong <gongqifeng.vip@gmail.com>
2018-07-23 09:06:15 +0800

    using attribute to Builder

---
+++ src/Builder/Attributed/BuildStepAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PracticalPatterns.Builder.Attributed
+{
+    [AttributeUsage(AttributeTargets.Method,AllowMultiple=false)]
+    public sealed class BuildStepAttribute:Attribute
+    {
+        private int _sequence;
+        private int _times;
+        public int Sequence { get { return this._sequence; } }
+        public int Times { get { return this._times; } }
+
+        public BuildStepAttribute(int sequence, int times)
+        {
+            if ((sequence <= 0) || (times <= 0))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            this._sequence = sequence;
+            this._times = times;
+        }
+
+        public BuildStepAttribute(int sequence) : this(sequence, 1) { }
+    }
+}
---
+++ src/Builder/Attributed/BuilderStepDiscovery.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace PracticalPatterns.Builder.Attributed
+{
+    public class BuildStep
+    {
+        public MethodInfo Method { get; set; }
+        public int Times { get; set; }
+        public int Sequence { get; set; }
+    }
+
+    public class BuilderStepDiscovery
+    {
+        static IDictionary<Type, IEnumerable<BuildStep>> cache =
+            new Dictionary<Type, IEnumerable<BuildStep>>();
+
+        static IList<Type> errorCache = new List<Type>();
+
+        public IEnumerable<BuildStep> DiscoveryBuildSteps(Type type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException("type");
+            }
+            if (errorCache.Contains(type))
+            {
+                return null;
+            }
+            if (!cache.ContainsKey(type))
+            {
+                var aType = typeof(BuildStepAttribute);
+                var methods = from item in
+                                  (from method in type.GetMethods()
+                                   where method.IsDefined(aType, false)
+                                   select new
+                                   {
+                                       M = method,
+                                       A = (BuildStepAttribute)method.GetCustomAttributes(aType, false).First()
+                                   }
+                                  )
+                              orderby item.A.Sequence
+                              select new BuildStep
+                              {
+                                  Method = item.M,
+                                  Times = item.A.Times,
+                                  Sequence = item.A.Sequence
+                              };
+                if (methods.Count() == 0)
+                {
+                    errorCache.Add(type);
+                    return null;
+                }
+                else
+                {
+                    cache.Add(type, methods);
+                    return methods;
+                }
+            }
+            else
+                return cache[type];
+        }
+    }
+
+}
