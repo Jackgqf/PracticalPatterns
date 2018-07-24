f6e8b1a8e0ff7d0492063e30054589569b37f90a
qifeng gong <gongqifeng.vip@gmail.com>
2018-07-24 21:28:48 +0800

    Ptototype pattern

---
+++ src/Prototype/Classic/IPrototype.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PracticalPatterns.Prototype.Classic
+{
+    public class Indicator { }
+
+    public interface IPrototype
+    {
+        IPrototype Clone();
+        string Name { get; set; }
+        Indicator Signal { get; }
+    }
+}
