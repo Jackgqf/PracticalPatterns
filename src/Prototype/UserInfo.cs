ad8606e535aa744cb1c5b9a73e023ebe0438774e
qifeng gong <gongqifeng.vip@gmail.com>
2018-07-26 15:56:46 +0800

    deep coping by serialization

---
+++ src/Prototype/UserInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace PracticalPatterns.Prototype
+{
+    [Serializable]
+    public class UserInfo : ISerializable
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string[] Education { get; set; }
+
+        public UserInfo() { }
+
+        protected UserInfo(SerializationInfo info, StreamingContext context)
+        {
+            Name = info.GetString("Name");
+            Education = (string[])info.GetValue("Education", typeof(string[]));
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Name", this.Name);
+            info.AddValue("Education", Education.Take(3).ToArray());
+        }
+    }
+}
---
+++ unitTest/Prototype.Tests/SerializationFixture.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PracticalPatterns.Common;
+
+namespace PracticalPatterns.Prototype.Tests
+{
+    [TestClass]
+    public class SerializationFixture
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var user = new UserInfo()
+            {
+                Name = "Joe",
+                Age = 10,
+                Education = new string[] { "A", "B", "C", "D" }
+            };
+
+            var graph = SerializationHelper.SerializeObjectToString(user);
+            var clone = SerializationHelper.DeserializeStringToObject<UserInfo>(graph);
+            Assert.AreEqual<int>(3, clone.Education.Count());
+            Assert.AreNotEqual(user.Age, clone.Age);
+            Assert.AreEqual<string>(user.Name, clone.Name);
+        }
+    }
+}
