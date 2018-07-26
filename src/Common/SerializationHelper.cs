2b68cba6425b6dff31b7a9048245a11c3f32f8dd
qifeng gong <gongqifeng.vip@gmail.com>
2018-07-26 15:09:17 +0800

    a serialize class

---
+++ src/Common/SerializationHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.IO;
+
+namespace PracticalPatterns.Common
+{
+    public enum FormatterType
+    {
+        Soap,
+        Binary
+    }
+
+    public static class SerializationHelper
+    {
+        const FormatterType DefaultFormatterType = FormatterType.Binary;
+
+        static IRemotingFormatter GetFormatter(FormatterType formatterType)
+        {
+            switch (formatterType)
+            {
+                case FormatterType.Soap:
+                    return new SoapFormatter();
+                case FormatterType.Binary:
+                    return new BinaryFormatter();
+            }
+            throw new NotSupportedException();
+        }
+
+        public static string SerializeObjectToString(object graph, FormatterType formatterType = DefaultFormatterType)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                var formatter = GetFormatter(formatterType);
+                formatter.Serialize(memoryStream, graph);
+                var arrGraph = memoryStream.ToArray();
+                return Convert.ToBase64String(arrGraph);
+            }
+        }
+
+        public static T DeserializeStringToObject<T>(string graph, FormatterType formatterType = DefaultFormatterType)
+        {
+            var arrGraph = Convert.FromBase64String(graph);
+            using (var memoryStream = new MemoryStream(arrGraph))
+            {
+                var formatter = GetFormatter(formatterType);
+                return (T)formatter.Deserialize(memoryStream);
+            }
+        }
+    }
+}
---
+++ unitTest/Common.Tests/SerializationHelperFixture.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Runtime.Serialization;
+
+namespace PracticalPatterns.Common.Tests
+{
+    [TestClass]
+    public class SerializationHelperFixture
+    {
+        [Serializable]
+        class UserInfo
+        {
+            public string Name;
+            public readonly IList<string> Education = new List<string>();
+
+            public UserInfo GetShallowCopy() { return (UserInfo)this.MemberwiseClone(); }
+            public UserInfo GetDeepCopy()
+            {
+                return SerializationHelper.DeserializeStringToObject<UserInfo>(SerializationHelper.SerializeObjectToString(this));
+            }
+        }
+
+        [TestMethod]
+        public void ShallowCopyTest()
+        {
+            var user1 = new UserInfo();
+            user1.Name = "joe";
+            user1.Education.Add("A");
+            var user2 = user1.GetShallowCopy();
+
+            Assert.AreEqual<string>(user1.Name, user2.Name);
+            Assert.AreEqual<string>(user1.Education[0], user2.Education[0]);
+            user2.Name = "Nancy";
+            user2.Education[0] = "B";
+            Assert.AreNotEqual<string>(user1.Name, user2.Name);
+            Assert.AreEqual<string>(user1.Education[0], user2.Education[1]);
+        }
+
+        [TestMethod]
+        public void DeepCopyTest()
+        {
+            var user1 = new UserInfo();
+            user1.Name = "Joe";
+            user1.Education.Add("A");
+            var user2 = user1.GetDeepCopy();
+
+            Assert.AreEqual<string>(user1.Name, user2.Name);
+            Assert.AreEqual<string>(user1.Education[0], user2.Education[0]);
+
+            user2.Name = "Nancy";
+            user2.Education[0] = "B";
+            Assert.AreNotEqual<string>(user1.Name, user2.Name);
+            Assert.AreNotEqual<string>(user1.Education[0], user2.Education[0]);
+        }
+
+        class Inner { }
+        class Middle { Inner sub = new Inner();}
+        class Outer { Middle sub = new Middle();}
+
+        [Serializable]
+        class Client
+        {
+            Outer outer = new Outer();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(SerializationException))]
+        public void CannotSerialization()
+        {
+            //run error
+            var graph = SerializationHelper.SerializeObjectToString(new Client());
+        }
+
+        [Serializable]
+        class Parent { }
+
+        [Serializable]
+        class Teacher { }
+
+        [Serializable]
+        class Child
+        {
+            [NonSerialized]
+            public Parent[] Parents;
+
+            public Teacher Teacher { get; set; }
+        }
+
+        [TestMethod]
+        public void Test()
+        {
+            var child = new Child();
+            child.Teacher = new Teacher();
+            child.Parents = new Parent[2];
+            Assert.IsNotNull(child.Parents);
+            Assert.IsNotNull(child.Teacher);
+
+            var clone = SerializationHelper.DeserializeStringToObject<Child>(SerializationHelper.SerializeObjectToString(child));
+            Assert.IsNull(clone.Parents);
+            Assert.IsNotNull(clone.Teacher);
+        }
+    }
+}
