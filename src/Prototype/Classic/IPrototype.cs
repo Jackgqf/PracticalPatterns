fea85c386b53c4c05da764dbe4fb42121e830a1d
qifeng gong <gongqifeng.vip@gmail.com>
2018-07-26 11:06:14 +0800

    the default clone in C#

--- src/Prototype/Classic/IPrototype.cs
+++ src/Prototype/Classic/IPrototype.cs
@@ -10,7 +10,22 @@ namespace PracticalPatterns.Prototype.Classic
     public interface IPrototype
     {
         IPrototype Clone();
-        string Name { get; set; }
-        Indicator Signal { get; }
+        string Name { get; set; }// value clone
+        Indicator Signal { get; }// ref clone
+    }
+
+    public class ConcretePrototype : IPrototype
+    {
+        private string name = string.Empty;
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        private Indicator _signal = new Indicator();
+        public Indicator Signal { get { return this._signal; } }
+
+        public IPrototype Clone() { return (IPrototype)this.MemberwiseClone(); }
     }
 }
---
+++ unitTest/Prototype.Tests/ClassicTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PracticalPatterns.Prototype.Classic;
+
+namespace PracticalPatterns.Prototype.Tests
+{
+    [TestClass]
+    public class ClassicTest
+    {
+        IPrototype sample;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            sample = new ConcretePrototype();
+        }
+
+        [TestMethod]
+        public void Test()
+        {
+            var mirrorImage = sample.Clone();
+            Assert.AreEqual<int>(sample.Signal.GetHashCode(), mirrorImage.Signal.GetHashCode());
+            Assert.AreEqual<string>(mirrorImage.Name, sample.Name);
+
+            sample.Name = "A";
+            Assert.AreNotEqual<string>("A", mirrorImage.Name);
+            mirrorImage = sample.Clone();
+            Assert.AreEqual<string>("A",mirrorImage.Name);
+            Assert.IsInstanceOfType(mirrorImage,typeof(ConcretePrototype));   
+        }
+    }
+}
